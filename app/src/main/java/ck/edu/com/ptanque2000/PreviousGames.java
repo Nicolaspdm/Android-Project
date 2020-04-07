4afa4818dd7e61d3dce1df3e21d650c3a6c632f8
Nicolaspdm <nicolas.prince@edu.ece.fr>
2020-04-07 18:35:57 +0200

    Création de l'activité prévious game avec Une Custom ListView

---
+++ app/src/main/java/ck/edu/com/ptanque2000/PreviousGames.java
@@ -0,0 +1,43 @@
+package ck.edu.com.ptanque2000;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class PreviousGames extends AppCompatActivity {
+
+    ListView list;
+
+    String[] winner_name = {
+            "Arnaud", "Henri", "Jade",
+            "Seb", "Nico",
+    };
+
+    String[] score_value ={
+            "10", "12", "13",
+            "7", "13",
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_previous_games);
+
+
+        MyListAdapter adapter = new MyListAdapter(this, winner_name, score_value);
+        list = findViewById(R.id.list);
+        list.setAdapter(adapter);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(PreviousGames.this, MainActivity.class);
+        startActivity(intent);}
+
+}
