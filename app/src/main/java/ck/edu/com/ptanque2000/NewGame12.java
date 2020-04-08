06a93e553b8b0b941adb8bb4f88d30766bab3727
JadouECE <jade.laville@edu.ece.fr>
2020-04-08 20:04:57 +0200

    NewSet Error

--- app/src/main/java/ck/edu/com/ptanque2000/NewGame12.java
+++ app/src/main/java/ck/edu/com/ptanque2000/NewGame12.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class NewGame12 extends AppCompatActivity {
 
     private String player1;
     private String player2;
-
+    private TextView joueur1;
+    private TextView joueur2;
+    private Button ajout_set;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,27 @@ public class NewGame12 extends AppCompatActivity {
         Intent i = getIntent();
         player1=i.getStringExtra("p1");
         player2=i.getStringExtra("p2");
+
+        joueur1.findViewById(R.id.joueur1_12);
+        joueur2.findViewById(R.id.joueur2_12);
+
+        joueur1.setText(player1);
+        joueur2.setText(player2);
+
+        ajout_set.findViewById(R.id.add_set);
+        ajout_set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = player1;
+                String data2 = player2;
+
+                Intent intent4 = new Intent(NewGame12.this,NewSet.class);
+                intent4.putExtra("p1", data);
+                intent4.putExtra("p2",data2);
+                startActivity(intent4);
+            }
+        });
+
 
 
     }
