fa54204e816637b92fb852d8066c0e16a4a66d2b
JadouECE <jade.laville@edu.ece.fr>
2020-04-11 23:41:01 +0200

    Update NewGame11.java

--- app/src/main/java/ck/edu/com/ptanque2000/NewGame11.java
+++ app/src/main/java/ck/edu/com/ptanque2000/NewGame11.java
@@ -62,8 +62,10 @@ public class NewGame11 extends AppCompatActivity {
                 String data2 = player2.getText().toString();
                 String datalt = latitude.getText().toString();
                 String datalg = longitude.getText().toString();
+                String indice = "NG11";
 
                 Intent intent3 = new Intent(NewGame11.this,NewGame12.class);
+                intent3.putExtra("indice",indice);
                 intent3.putExtra("p1",data);
                 intent3.putExtra("p2", data2);
                 intent3.putExtra("latitude",datalt);
