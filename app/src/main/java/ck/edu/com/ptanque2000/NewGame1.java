f077aa0812855968e49545db78977d30f9c4ce7d
JadouECE <jade.laville@edu.ece.fr>
2020-04-11 18:50:13 +0200

    implement no fonctionnel encore

--- app/src/main/java/ck/edu/com/ptanque2000/NewGame1.java
+++ app/src/main/java/ck/edu/com/ptanque2000/NewGame1.java
@@ -5,17 +5,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class NewGame1 extends AppCompatActivity {
+public class NewGame1 extends AppCompatActivity implements LocationListener {
 
 
     private EditText joueur1;
     private EditText joueur2;
     private EditText nb_joueurs;
+    private TextView latitude;
+    private TextView longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +40,8 @@ public class NewGame1 extends AppCompatActivity {
         joueur1=findViewById(R.id.nameJ1);
         joueur2=findViewById(R.id.nameJ2);
         nb_joueurs=findViewById(R.id.numberJ);
+        latitude=findViewById(R.id.lati_ng1);
+        longitude=findViewById(R.id.longi_ng1);
 
         Button button_start = findViewById(R.id.btn_start);
         button_start.setOnClickListener(new View.OnClickListener() {
