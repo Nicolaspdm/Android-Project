e8c8cce795aef2f4b368cd0a9180726427b5afd1
JadouECE <jade.laville@edu.ece.fr>
2020-04-12 13:00:29 +0200

    avancment supprsion class new set

--- app/src/main/java/ck/edu/com/ptanque2000/NewGame12.java
+++ app/src/main/java/ck/edu/com/ptanque2000/NewGame12.java
@@ -7,6 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,10 +24,19 @@ public class NewGame12 extends AppCompatActivity {
     private String score;
     private TextView joueur1;
     private TextView joueur2;
+    private TextView j1_ns;
+    private TextView j2_ns;
+    private Switch switch_ns;
+    private EditText score_ns;
     private TextView latitude;
     private TextView longitude;
     private Button ajout_set;
 
+    ListView list_ng12;
+
+    String[] winner_name_ng12 = null;
+
+    String[] score_value_ng12 = null;
 
 
 
@@ -36,10 +48,18 @@ public class NewGame12 extends AppCompatActivity {
         }
         setContentView(R.layout.activity_new_game12);
 
+        MyListAdapt_NG12 adapter_ng12 = new MyListAdapt_NG12(this, winner_name_ng12, score_value_ng12);
+        list_ng12 = findViewById(R.id.list_ng12);
+        list_ng12.setAdapter(adapter_ng12);
+
         joueur1 = (TextView) findViewById(R.id.joueur1_12);
+        j1_ns = (TextView)findViewById(R.id.ns_j1);
+        j2_ns = (TextView) findViewById(R.id.ns_j2);
+        switch_ns = (Switch) findViewById(R.id.ns_swich);
         joueur2 = (TextView) findViewById(R.id.joueur2_12);
         longitude = (TextView) findViewById(R.id.longitude_ng12);
         latitude = (TextView) findViewById(R.id.latitude_ng12);
+        score_ns = (EditText) findViewById(R.id.nvx_score);
 
         Intent i = getIntent();
         indice=i.getStringExtra("indice");
@@ -47,45 +67,24 @@ public class NewGame12 extends AppCompatActivity {
         player2=i.getStringExtra("p2");
         lat=i.getStringExtra("latitude");
         longi=i.getStringExtra("longitude");
-        //winner=i.getStringExtra("winner");
-
-
-    /*if(savedInstanceState != null){
-
-        ///RECUPERATION DES STATES
-        player1 = savedInstanceState.getString("J1");
-        player2 = savedInstanceState.getString("J2");
-        lat = savedInstanceState.getString("latitude");
-        longi = savedInstanceState.getString("longitude");
-    }*/
 
             joueur1.setText(player1);
             joueur2.setText(player2);
+            j1_ns.setText(player1);
+            j2_ns.setText(player2);
             longitude.setText(longi);
             latitude.setText(lat);
-
 
         /// BTN AJOUT SET
         ajout_set = findViewById(R.id.add_set);
         ajout_set.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String data = player1;
-                String data2 = player2;
+                if(score_ns.getText().toString().equals("")){
+                    Toast.makeText(NewGame12.this,"Entrez un score svp", Toast.LENGTH_SHORT).show();
+                }
+                
 
-                ///SAVE STATE
-               /* if(player1!=null && player2 != null && lat != null && longi != null) {
-                    savedInstanceState.putString("J1", player1);
-                    savedInstanceState.putString("J2", player2);
-                    savedInstanceState.putString("latitude", lat);
-                    savedInstanceState.putString("longitude", longi);
-                }*/
-                ///INTENT NEW SET
-               // NewGame12.this.onSaveInstanceState(savedInstanceState);
-                Intent intent4 = new Intent(NewGame12.this,NewSet.class);
-                intent4.putExtra("p1", data);
-                intent4.putExtra("p2",data2);
-                startActivity(intent4);
             }
         });
 
