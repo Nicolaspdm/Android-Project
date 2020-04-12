39df58b7643aa080dbdfba21946bf00a32419722
JadouECE <jade.laville@edu.ece.fr>
2020-04-12 02:02:52 +0200

    Attnte bdd

--- app/src/main/java/ck/edu/com/ptanque2000/NewGame12.java
+++ app/src/main/java/ck/edu/com/ptanque2000/NewGame12.java
@@ -1,6 +1,7 @@
 package ck.edu.com.ptanque2000;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -24,37 +25,47 @@ public class NewGame12 extends AppCompatActivity {
     private TextView longitude;
     private Button ajout_set;
 
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (savedInstanceState != null){
+            NewGame12.this.onRestoreInstanceState(savedInstanceState);
+        }
         setContentView(R.layout.activity_new_game12);
-
-        Intent i = getIntent();
-        try{indice=i.getStringExtra("indice");}catch (Error e){Toast.makeText(NewGame12.this, "indice erreur", Toast.LENGTH_SHORT).show();}
-
-        //si intent vient de NG11 alors on récupère les bonnes données
-        //if(indice=="NG11"){
-            try{player1=i.getStringExtra("p1");}catch (Error e){Toast.makeText(NewGame12.this, "player1 erreur", Toast.LENGTH_SHORT).show();}
-            try{player2=i.getStringExtra("p2");}catch (Error e){Toast.makeText(NewGame12.this, "player2 erreur", Toast.LENGTH_SHORT).show();}
-            try{lat=i.getStringExtra("latitude");}catch (Error e){Toast.makeText(NewGame12.this, "latitude erreur", Toast.LENGTH_SHORT).show();}
-            try{longi=i.getStringExtra("longitude");}catch (Error e){Toast.makeText(NewGame12.this, "longueur erreur", Toast.LENGTH_SHORT).show();}
-       // }
-       // else if (indice=="NS"){
-            try{winner=i.getStringExtra("winner");}catch (Error e){Toast.makeText(NewGame12.this, "winner erreur", Toast.LENGTH_SHORT).show();}
-            try{score=i.getStringExtra("score");}catch (Error e){Toast.makeText(NewGame12.this, "score erreur", Toast.LENGTH_SHORT).show();}
-
-        //}
 
         joueur1 = (TextView) findViewById(R.id.joueur1_12);
         joueur2 = (TextView) findViewById(R.id.joueur2_12);
         longitude = (TextView) findViewById(R.id.longitude_ng12);
         latitude = (TextView) findViewById(R.id.latitude_ng12);
 
-        joueur1.setText(player1);
-        joueur2.setText(player2);
-        longitude.setText(longi);
-        latitude.setText(lat);
+        Intent i = getIntent();
+        indice=i.getStringExtra("indice");
+        player1=i.getStringExtra("p1");
+        player2=i.getStringExtra("p2");
+        lat=i.getStringExtra("latitude");
+        longi=i.getStringExtra("longitude");
+        //winner=i.getStringExtra("winner");
 
+
+    /*if(savedInstanceState != null){
+
+        ///RECUPERATION DES STATES
+        player1 = savedInstanceState.getString("J1");
+        player2 = savedInstanceState.getString("J2");
+        lat = savedInstanceState.getString("latitude");
+        longi = savedInstanceState.getString("longitude");
+    }*/
+
+            joueur1.setText(player1);
+            joueur2.setText(player2);
+            longitude.setText(longi);
+            latitude.setText(lat);
+
+
+        /// BTN AJOUT SET
         ajout_set = findViewById(R.id.add_set);
         ajout_set.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,6 +73,15 @@ public class NewGame12 extends AppCompatActivity {
                 String data = player1;
                 String data2 = player2;
 
+                ///SAVE STATE
+               /* if(player1!=null && player2 != null && lat != null && longi != null) {
+                    savedInstanceState.putString("J1", player1);
+                    savedInstanceState.putString("J2", player2);
+                    savedInstanceState.putString("latitude", lat);
+                    savedInstanceState.putString("longitude", longi);
+                }*/
+                ///INTENT NEW SET
+               // NewGame12.this.onSaveInstanceState(savedInstanceState);
                 Intent intent4 = new Intent(NewGame12.this,NewSet.class);
                 intent4.putExtra("p1", data);
                 intent4.putExtra("p2",data2);
@@ -71,6 +91,31 @@ public class NewGame12 extends AppCompatActivity {
 
 
 
+
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString("J1", player1);
+        savedInstanceState.putString("J2", player2);
+        savedInstanceState.putString("latitude", lat);
+        savedInstanceState.putString("longitude", longi);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        player1 = savedInstanceState.getString("J1");
+        joueur1.setText(player1);
+        player2 = savedInstanceState.getString("J2");
+        joueur2.setText(player2);
+        lat = savedInstanceState.getString("latitude");
+        latitude.setText(lat);
+        longi = savedInstanceState.getString("longitude");
+        longitude.setText(longi);
+
+    }
+
 
 }
